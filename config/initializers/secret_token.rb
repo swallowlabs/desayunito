eb1f95e8257a7c87e13bc373a07c0a84b8b91392
tangzero <t4ngz3r0@gmail.com>
2010-09-07 14:10:39 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Desayunito::Application.config.secret_token = '22d01e9c98a15e478e4d58d399187b89c3947c2c8710abf91bce00230a6198d508f43234f339a12f7b2cf74f50bd1317b2d657eaa665b0a14dbf2208f3069f63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Desayunito::Application.config.session_store :cookie_store, :key => '_desayunito_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Desayunito::Application.config.session_store :active_record_store
